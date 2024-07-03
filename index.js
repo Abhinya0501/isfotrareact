7a2afda0279eedb6b8781fd584a090c6ff36170d
abhinya0501 <singhrajputabhishek1234@gmail.com>
2024-07-03 15:33:09 +0530

    testing

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main thing
+
+app.use(express.json()); // to accept json data
+
+app.use("/api/users", userRoutes);
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running on port ${PORT}..`
+  )
+);
